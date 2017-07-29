c1798a51c228afd2a0cb03b99b641bdb197b608b
Ivan Zotov <ivanzotov@gmail.com>
2017-07-29 22:22:02 +0300

    Add ios project

---
+++ ios/RNTextInputMask/RNTextInputMask.h
@@ -0,0 +1,19 @@
+//
+//  RNTextInputMask.h
+//  RNTextInputMask
+//
+//  Created by Ivan Zotov on 7/29/17.
+//
+//
+
+#ifndef RNTextInputMask_h
+#define RNTextInputMask_h
+
+#import <UIKit/UIKit.h>
+#import "RCTBridgeModule.h"
+
+@interface RNTextInputMask : NSObject <RCTBridgeModule>
+@end
+
+
+#endif /* RNTextInputMask_h */
---
+++ ios/RNTextInputMask/RNTextInputMask.m
@@ -0,0 +1,29 @@
+#import "RCTBridge.h"
+#import "RCTConvert.h"
+#import "RCTTextField.h"
+#import "RCTUIManager.h"
+#import "RCTEventDispatcher.h"
+#import "Carriage.h"
+
+@implementation RNTextInputMask
+  @synthesize bridge = _bridge;
+
+  RCT_EXPORT_MODULE();
+
+  - (dispatch_queue_t)methodQueue {
+      return self.bridge.uiManager.methodQueue;
+  }
+
+  RCT_EXPORT_METHOD(setMask:(nonnull NSNumber *)reactNode mask:(NSString *)mask) {
+    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry ) {
+      UIView *view = viewRegistry[reactNode];
+      RCTTextField *textView = ((RCTTextField *)view);
+
+      dispatch_async(dispatch_get_main_queue(), ^{
+        MaskedTextFieldDelegate *maskedDelegate = [MaskedTextFieldDelegate format:mask]
+        maskedDelegate.listener = self
+        textView.delegate = maskedDelegate
+      });
+    }];
+  }
+@end
