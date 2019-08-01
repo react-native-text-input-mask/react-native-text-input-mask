e475b64c146cd382a74a884689bdbea59ea40bf6
Ivan Zotov <git@ivanzotov.com>
2019-08-01 17:47:23 +0300

    Improve ios native code to delegate original method

--- ios/RNTextInputMask.m
+++ ios/RNTextInputMask.m
@@ -1,8 +1,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTConvert.h>
 #import <React/RCTUIManager.h>
-#import <React/RCTEventDispatcher.h>
-#import <RCTText/RCTSinglelineTextInputView.h>
+#import <RCTText/RCTBaseTextInputView.h>
 #import <RCTText/RCTUITextField.h>
 #import "RNTextInputMask.h"
 
@@ -31,9 +30,9 @@ RCT_EXPORT_METHOD(unmask:(NSString *)maskString inputValue:(NSString *)inputValu
 }
 
 RCT_EXPORT_METHOD(setMask:(nonnull NSNumber *)reactNode mask:(NSString *)mask) {
-    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, NSDictionary<NSNumber *, RCTSinglelineTextInputView *> *viewRegistry ) {
+    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, NSDictionary<NSNumber *, RCTBaseTextInputView *> *viewRegistry ) {
         dispatch_async(dispatch_get_main_queue(), ^{
-            RCTSinglelineTextInputView *view = viewRegistry[reactNode];
+            RCTBaseTextInputView *view = viewRegistry[reactNode];
             RCTUITextField *textView = [view backedTextInputView];
 
             if (!masks) {
@@ -43,65 +42,10 @@ RCT_EXPORT_METHOD(setMask:(nonnull NSNumber *)reactNode mask:(NSString *)mask) {
             NSString *key = [NSString stringWithFormat:@"%@", reactNode];
             MaskedTextFieldDelegate* maskedDelegate = [MaskedTextFieldDelegate new];
             maskedDelegate.primaryMaskFormat = mask;
-            maskedDelegate.listener = self;
+            maskedDelegate.listener = textView.delegate;
             masks[key] = maskedDelegate;
             textView.delegate = masks[key];
-
-            [self updateTextField:maskedDelegate textView:textView];
         });
     }];
 }
-
-- (void)textField:(RCTUITextField *)textField didFillMandatoryCharacters:(BOOL)complete didExtractValue:(NSString *)value
-{
-    [self.bridge.eventDispatcher sendTextEventWithType:RCTTextEventTypeChange
-                                              reactTag:[[textField reactSuperview] reactTag]
-                                                  text:textField.attributedText.string
-                                                   key:nil
-                                            eventCount:1];
-}
-
-- (void)textFieldDidBeginEditing:(UITextField *)textField
-{
-    [self.bridge.eventDispatcher sendTextEventWithType:RCTTextEventTypeFocus
-                                              reactTag:[[textField reactSuperview] reactTag]
-                                                  text:textField.attributedText.string
-                                                   key:nil
-                                            eventCount:1];
-}
-
-- (void)textFieldDidEndEditing:(RCTUITextField *)textField
-{
-    [self.bridge.eventDispatcher sendTextEventWithType:RCTTextEventTypeBlur
-                                              reactTag:[[textField reactSuperview] reactTag]
-                                                  text:textField.attributedText.string
-                                                   key:nil
-                                            eventCount:1];
-}
-
-- (BOOL)textFieldShouldReturn:(RCTUITextField *)textField
-{
-    [self.bridge.eventDispatcher sendTextEventWithType:RCTTextEventTypeSubmit
-                                              reactTag:[[textField reactSuperview] reactTag]
-                                                  text:textField.attributedText.string
-                                                   key:nil
-                                            eventCount:1];
-    return YES;
-}
-
-
-- (void)updateTextField:(MaskedTextFieldDelegate *)maskedDelegate textView:(RCTUITextField *)textView {
-    if(textView.attributedText.string.length> 0){
-        NSString *originalString = textView.attributedText.string;
-        NSString *croppedText = [originalString substringToIndex:[originalString length] -1];
-
-        [textView setAttributedText:[[NSAttributedString alloc] initWithString:croppedText]];
-        NSString *last = [originalString substringFromIndex:[originalString length] - 1];
-
-        [maskedDelegate textField:(UITextField*)textView
-    shouldChangeCharactersInRange: (NSRange){[textView.attributedText.string length], 0}
-                replacementString:last];
-    }
-}
-
 @end
