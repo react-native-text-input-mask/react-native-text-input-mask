40f929ad938094f356b4cfd4ce32c03efe71de2d
Martin Treurnicht <martin@step.com>
2021-01-28 16:09:53 -0800

    Add autocomplete/autoskip cusomtization support, switch to async/await

--- ios/RNTextInputMaskBridge.m
+++ ios/RNTextInputMaskBridge.m
@@ -2,10 +2,19 @@
 
 @interface RCT_EXTERN_MODULE(RNTextInputMask, NSObject)
 
-RCT_EXTERN_METHOD(mask:(NSString *)maskString inputValue:(NSString *)inputValue onResult:(RCTResponseSenderBlock)onResult)
+RCT_EXTERN_METHOD(mask:(NSString *)maskString
+                  inputValue:(NSString *)inputValue
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(unmask:(NSString *)maskString inputValue:(NSString *)inputValue onResult:(RCTResponseSenderBlock)onResult)
+RCT_EXTERN_METHOD(unmask:(NSString *)maskString
+                  inputValue:(NSString *)inputValue
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(setMask:(nonnull NSNumber *)reactNode mask:(NSString *)mask)
+RCT_EXTERN_METHOD(setMask:(nonnull NSNumber *)reactNode
+                  mask:(NSString *)mask
+                  autocomplete:(BOOL *)autocomplete
+                  autoskip:(BOOL *)autoskip)
 
 @end
